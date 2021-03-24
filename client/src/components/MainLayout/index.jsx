1e4ecaefcd8bd21f2797dfd102f61611a47c19e1
Azizbek <azizbeksavkimov@mail.ru>
2021-03-24 22:58:46 +0500

    feat: add sidebar

--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AuthHeader from '../Header/AuthHeader/AuthHeader';
 import MainHeader from '../Header/MainHeader/MainHeader';
+import Sidebar from '../Header/Sidebar/Sidebar';
 
 export default function MainLayout({ children }) {
-  const [token, setToken] = useState('');
+  const location = useLocation();
+  const [token, setToken] = useState('a');
   return (
     <>
       <header>
         { token ? <MainHeader /> : <AuthHeader /> }
       </header>
+      { token ? <Sidebar /> : <></> }
       <main className="body">
         {children}
       </main>
